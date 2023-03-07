0faa24bf2b9417c9342e1f46b23bd8cd4e370b1d
zhang'song <201250190@smail.nju.edu.cn>
2023-03-07 22:24:15 +0800

    homework

---
+++ 作业/src/main/java/contact/ContactCloudApplication.java
@@ -0,0 +1,12 @@
+package contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   // <1>
+public class ContactCloudApplication {
+
+	public static void main(String[] args) {SpringApplication.run(ContactCloudApplication.class, args); // <2>
+		 }
+
+}
---
+++ 作业/src/main/java/contact/Message.java
@@ -0,0 +1,31 @@
+package contact;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+public class Message { //保存的信息是一个二维数组，message是内维
+
+	@NotBlank(message = "first name is required")
+	private String firstName;
+
+	@NotBlank(message = "last name is required")
+	private String lastName;
+
+	@NotBlank(message = "手机号码不能为空")
+	@NotNull(message = "手机号不能为空")
+	@Length(min = 11, max = 11, message = "手机号只能为11位")
+	@Pattern(regexp = "^[1][3,4,5,6,7,8,9][0-9]{9}$", message = "手机号格式有误")
+	private String phoneNumber;
+
+	@NotBlank(message = "email is required")
+	@Pattern(regexp = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$", message = "input correct email")
+	private String email;
+
+
+}
